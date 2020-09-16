c25f347b9bb85e9e5799269cf092e7f3e908870d
Js <190261036@qq.com>
2020-09-16 10:37:39 +0800

    1.0.1 修改包名

---
+++ app/src/main/java/com/smart/smart/ui/demo/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.smart.smart.ui.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(view: View) {
+        when(view.id){
+            R.id.sl1 ->{
+
+            }
+            R.id.sl2 ->{
+
+            }
+            R.id.sl3 ->{
+
+            }
+            R.id.sl4,R.id.st4  ->{
+                view.isEnabled = false
+            }
+        }
+    }
+
+}
